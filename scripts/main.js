f53b1223f6776bf8ba85981bf1df69e19fae6e1e
julie ugolotti <julie.ugolotti@gmail.com>
2021-04-07 17:41:11 -0400

    Adding a sucess message on the right time

--- scripts/main.js
+++ scripts/main.js
@@ -41,9 +41,11 @@ window.addEventListener('DOMContentLoaded', function() {
       }
 
       // If we've matched all the cards, display a message.
-      if (matchedCards.length > cards.length) {
-        alert('You matched all the cards, nice job!');
-      }
+      setTimeout(() => {
+        if (matchedCards.length == cards.length) {
+          alert('You matched all the cards, nice job!');
+          }
+        }, 100)
     });
   });
 
